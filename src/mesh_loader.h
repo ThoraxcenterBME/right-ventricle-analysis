a71b2944050cb8e815b926d740ae6dea2371ad04
victoriayuechen <victoriayuechen@gmail.com>
2024-03-31 10:00:09 +0200

    Change files on git

--- src/mesh_loader.h
+++
@@ -1,6 +0,0 @@
-#include <framework/mesh.h>
-
-Mesh loadMeshRV(std::istream& in);
-void loadRingFromFile(std::string& name, std::vector<Vertex>& vertices);
-void mark_excluded(std::string& in, std::vector<Vertex>& vertices); 
-void mark_regions(std::string& in, std::vector<Vertex>& vertices); 
