96c86d57414e487cc412f65caf8f9071028d8125
victoriayuechen <victoriayuechen@gmail.com>
2024-03-31 10:09:58 +0200

    Add frame work and change README

--- README.md
+++ README.md
@@ -1,13 +1,14 @@
-# Right Ventricle Analysis 
+# Right Ventricle Echo Analysis 
+This repository contains code to extract curvature, volume, area, and directional strain from 3D mesh data obtained with echocardiography. 
+
+The interface is built using OpenGL and one can analyse one mesh at a time. Drawing functionalities such as coloring regions and viewing normals on the mesh is possible. Batch analysis for multiple cycles is possible using the calculation functionalities of the repository. This works with 3D mesh data created with TomTec. 
 
 ![](imgs/labeled-regions.png)
 
-This repository contains code to extract curvature, volume, area, and directional strain from 3D mesh data obtained with echocardiography. 
-
-GUI is built using OpenGL and one can analyse one mesh at a time using the GUI. Batch analysis for multiple cycles is possible using the calculation functionalities of the repository. This works with 3D mesh data created with TomTec. 
-
 ### Curvature comparison
+Comparison of right ventricle calculations for healthy controls and Tetrology of Fallot patients. 
 ![](imgs/end-diastole.png)
 
 ### GUI for analysis 
+The interface used for analysing one echo mesh at a time. 
 ![](imgs/GUI_new.png)
---
+++ framework/include/framework/mesh.h
@@ -0,0 +1,84 @@
+#pragma once
+#include "image.h"
+// Suppress warnings in third-party code.
+#include <framework/disable_all_warnings.h>
+DISABLE_WARNINGS_PUSH()
+#include <glm/vec2.hpp>
+#include <glm/vec3.hpp>
+DISABLE_WARNINGS_POP()
+#include <filesystem>
+#include <optional>
+#include <span>
+#include <vector>
+#include <map>
+
+// Enumerated type for indicating the region the vertex belongs to 
+enum Region {
+	// IFW = Inferior Free Wall
+	// LFW = Laterior Free Wall
+	// AFW = Anterior Free Wall 
+	// SP = Septal body
+	// IGN = Ignored 
+	// UD = Initial value, so we haven't classified it 
+	IFW=0, LFW=1, AFW=2, SP=3, IGN=4, UD=5
+};
+
+struct Vertex {
+	// Variables for vertex 
+	glm::vec3 position;
+	glm::vec3 normal;
+	glm::vec2 texCoord; // Texture coordinate
+    std::vector<int> ring; // Saves the vertices that make the 1-ring neighbourhood
+	int ringCount; 
+	double curvature;
+    double indexed_curv;
+    double long_strain; 
+	double rad_strain; 
+	double circ_strain; 
+    
+	bool exclude = false; // whether it should be excluded in the curvature calculation
+    [[nodiscard]] constexpr bool operator==(const Vertex&) const noexcept = default;
+	int index; 
+    Region region = Region::UD;  
+	
+	// Setters (might remove)
+	void setCurvature(double c) {
+		curvature = c; 
+	}
+	void set_index_curv(double c) {
+		indexed_curv = c; 
+	}
+	void set_region(Region r) {
+		region = r; 
+	}
+	double get_indexed_curvature() {
+		return indexed_curv; 
+	}
+};
+
+struct Material {
+	glm::vec3 kd; // Diffuse color.
+	glm::vec3 ks{ 0.0f };
+	float shininess{ 1.0f };
+	float transparency{ 1.0f };
+	std::shared_ptr<Image> kdTexture;
+};
+
+struct Mesh {
+	// Vertices contain the vertex positions and normals of the mesh.
+	std::vector<Vertex> vertices;
+	// A triangle contains a triplet of values corresponding to the indices of the 3 vertices in the vertices array.
+	std::vector<glm::uvec3> triangles;
+	Material material;
+    std::map<int, std::vector<int>> vertexToTri;
+    double circ_radius;
+    double rad_radius; 
+};
+
+[[nodiscard]] std::vector<Mesh> loadMesh(const std::filesystem::path& file, bool normalize = false);
+[[nodiscard]] Mesh mergeMeshes(std::span<const Mesh> meshes);
+void meshFlipX(Mesh& mesh);
+void meshFlipY(Mesh& mesh);
+void meshFlipZ(Mesh& mesh);
+
+
